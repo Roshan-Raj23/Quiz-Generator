606a05548cb66a8e7d3b0464fa14aa3f9d66b7cb
Roshan-Raj23 <roshanraj1543@gmail.com>
2025-07-11 23:48:38 +0530

    middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,53 +19,62 @@ export async function middleware(request: NextRequest) {
 }
 
 async function middlewareAuth(request: NextRequest) {
-  const user = await getUserFromSession(request.cookies) as User;
-  if (loginRequiredRoutes.includes(request.nextUrl.pathname)) {
-    if (user == null) {
-      return NextResponse.redirect(new URL("/signin", request.url))
+
+  try { 
+
+    const user = await getUserFromSession(request.cookies) as User;
+    if (loginRequiredRoutes.includes(request.nextUrl.pathname)) {
+      if (user == null) {
+        return NextResponse.redirect(new URL("/signin", request.url))
+      }
     }
+  
+    if (creatorRequiredRoutes.includes(request.nextUrl.pathname)) {
+      if (user == null) {
+        return NextResponse.redirect(new URL("/signin", request.url))
+      } else if (user.isCreator == false) {
+        return NextResponse.redirect(new URL("/", request.url))
+      }
+    }
+  
+    if (request.nextUrl.pathname.startsWith('/take-quiz/')) {
+      if (user == null) {
+        return NextResponse.redirect(new URL("/signin", request.url))
+      }
+  
+      const { pathname } = request.nextUrl;
+      const pathSegments = pathname.split('/');
+  
+      if (pathSegments.length < 3) {
+        return NextResponse.redirect(new URL("/take", request.url))
+      }
+      const id = pathSegments[2];
+  
+  
+      const url = request.nextUrl.clone();
+      url.pathname = '/api/isQuiz';
+
+      console.log(url);
+  
+      const response = await fetch(url , {
+          method: 'POST',
+          headers: {
+            cookie: request.headers.get('cookie') || '',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id }),
+        });
+      const data = await response.json();
+  
+      if (data.status !== 200 || !data.find){
+        
+        return NextResponse.redirect(new URL("/take?error=notFound", request.url));
+      }
+    }
+  } catch (err) {
+    console.error('Middleware error:', err)
   }
 
-  if (creatorRequiredRoutes.includes(request.nextUrl.pathname)) {
-    if (user == null) {
-      return NextResponse.redirect(new URL("/sign-in", request.url))
-    } else if (user.isCreator == false) {
-      return NextResponse.redirect(new URL("/", request.url))
-    }
-  }
-
-  if (request.nextUrl.pathname.startsWith('/take-quiz/')) {
-    if (user == null) {
-      return NextResponse.redirect(new URL("/signin", request.url))
-    }
-
-    const { pathname } = request.nextUrl;
-    const pathSegments = pathname.split('/');
-
-    if (pathSegments.length < 3) {
-      return NextResponse.redirect(new URL("/take", request.url))
-    }
-    const id = pathSegments[2];
-
-
-    const url = request.nextUrl.clone();
-    url.pathname = '/api/isQuiz';
-
-    const response = await fetch(url , {
-        method: 'POST',
-        headers: {
-          cookie: request.headers.get('cookie') || '',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id }),
-      });
-    const data = await response.json();
-
-    if (data.status !== 200 || !data.find){
-      
-      return NextResponse.redirect(new URL("/take?error=notFound", request.url));
-    }
-  }
 }
 
 export const config = {
